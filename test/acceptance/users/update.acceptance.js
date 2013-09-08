8b51531379b5d4259b84f964a5568bb328b55f03
Bryan Donovan <bdondo@gmail.com>
2013-09-07 20:27:06 -0700

    Test to prove we need to use CAS when updating records

--- test/acceptance/users/update.acceptance.js
+++ test/acceptance/users/update.acceptance.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var async = require('async');
 var support = require('../../support');
 
 function valid_params() {
@@ -19,10 +20,6 @@ describe("Feature: User updating", function () {
         http_client = support.http.client();
     });
 
-    after(function (done) {
-        http_client.del('/users/' + user.id, done);
-    });
-
     context("Given a user exists", function () {
         before(function (done) {
             params = valid_params();
@@ -32,6 +29,10 @@ describe("Feature: User updating", function () {
                 user = result.data;
                 done();
             });
+        });
+
+        after(function (done) {
+            http_client.del('/users/' + user.id, done);
         });
 
         context("When an api client PUTs to /users/:id with a new username and email", function () {
@@ -61,6 +62,63 @@ describe("Feature: User updating", function () {
                     done();
                 });
             });
+        });
+    });
+
+    context.skip("Scenario: Updating the same user multiple times, simultaneously", function () {
+        before(function (done) {
+            params = valid_params();
+
+            http_client.post('/users', params, function (err, result) {
+                assert.ifError(err);
+                user = result.data;
+                done();
+            });
+        });
+
+        after(function (done) {
+            http_client.del('/users/' + user.id, done);
+        });
+
+        it("Then the correct values should be saved each time", function (done) {
+            var nbr_of_runs = 10;
+            var runs = [];
+
+            for (var i = 0; i < nbr_of_runs; i++) {
+                runs.push(i);
+            }
+
+            async.eachSeries(runs, function (run, async_cb) {
+                var username = support.random.string();
+                var email = support.random.email();
+
+                async.parallel({
+                    update_username: function (parallel_cb) {
+                        var update_params = {
+                            username: username
+                        };
+
+                        http_client.put('/users/' + user.id, update_params, parallel_cb);
+                    },
+
+                    update_email: function (parallel_cb) {
+                        var update_params = {
+                            email: email
+                        };
+
+                        http_client.put('/users/' + user.id, update_params, parallel_cb);
+                    },
+                }, function (err) {
+                    if (err) { return async_cb(err); }
+
+                    http_client.get('/users/' + user.id, function (err, result) {
+                        assert.ifError(err);
+                        assert.equal(result.data.username, username);
+                        assert.equal(result.data.email, email);
+                        async_cb();
+                    });
+                });
+            }, done);
         });
     });
 });
